6772dbd59a334cf1fdad1a148550b885ae4e7855
Javier <javier.arredondo.c@usach.cl>
2021-04-09 11:58:00 -0400

    update requirements

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("requirements.txt") as f:
 required_packages = [r for r in required_packages if "-e" not in r]
 
 setup(
-    name="ztf_dr_downloader",
+    name="ztf_dr",
     version="1.0.0",
     description="",
     author="ALeRCE Team",
     author_email='contact@alerce.online',
-    packages=["ztf_dr_downloader"],
+    packages=["ztf_dr"],
     scripts=["cli/dr"],
     install_requires=required_packages
 )
